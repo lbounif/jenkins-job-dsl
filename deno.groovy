d44d21e49d5899fcd15ca8ff76905fc9abe55e22
lbounif <51320902+lbounif@users.noreply.github.com>
2021-10-25 23:39:19 +0100

    Create deno.groovy

---
+++ deno.groovy
@@ -0,0 +1,27 @@
+pipelineJob('deno app') { 
+
+  def repo = 'https://github.com/lbounif/deno-app' 
+
+  description("Deno app") 
+  keepDependencies(false) 
+
+  properties{ 
+
+    githubProjectUrl (repo) 
+    rebuild { 
+      autoRebuild(false) 
+    } 
+  } 
+
+  definition { 
+
+    cpsScm { 
+      scm { 
+        git { 
+          branches('master') 
+          scriptPath('Jenkinsfile') 
+          extensions { }  // required as otherwise it may try to tag the repo, which you may not want 
+        } 
+      } 
+    } 
+  }
